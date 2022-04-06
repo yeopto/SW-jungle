7bc9c62b5adddf068859f9e67b6e18be3397b59c
yeopto <gunyeop530@gmail.com>
2022-04-07 00:00:19 +0900

    2022-04-07

--- WEEK01/2438-별 찍기-1.py
+++ WEEK01/2438-별 찍기-1.py
@@ -1,6 +1,15 @@
-import sys
+# import sys
 
-N = int(sys.stdin.readline())
+# N = int(sys.stdin.readline())
 
-for i in range(1, N + 1):
-  print('*' * i)
+# for i in range(1, N + 1):
+#   print('*' * i)
+
+def rec(n):
+  if n == 0:
+    return
+  rec(n - 1)
+  print('*' * n)
+
+n = int(input())
+rec(n)
--- WEEK01/2577-숫자의 개수.py
+++ WEEK01/2577-숫자의 개수.py
@@ -2,7 +2,7 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-result = list(str(a * b * c))
+result = str(a * b * c)
 
 for i in range(10):
   print(result.count(str(i)))
